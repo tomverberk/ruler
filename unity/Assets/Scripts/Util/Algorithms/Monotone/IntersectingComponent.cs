2a300a4753d7407c33ff1f45f3742f933eb89562
Kevin Jilissen <info@kevinjilissen.nl>
2021-01-02 23:59:39 +0100

    Start MakeMonotone implementation.

---
+++ unity/Assets/Scripts/Util/Algorithms/Monotone/IntersectingComponent.cs
@@ -0,0 +1,26 @@
+﻿namespace Util.Monotone
+{
+  using System;
+  using Puzzle;
+
+  /// <summary>
+  /// Data structure for storing thet iontersecting components of the MakeMonotone
+  /// status structure. Comparison is implemented based on the x-coordinate of the
+  /// ??? TODO: sort on?
+  /// </summary>
+  class IntersectingComponent : IComparable<IntersectingComponent>, IEquatable<IntersectingComponent>
+  {
+    public PolygonEdge edge;
+    public PolygonPoint helper;
+
+    public int CompareTo(IntersectingComponent other)
+    {
+      return 0;
+    }
+
+    public bool Equals(IntersectingComponent other)
+    {
+      return edge.Equals(other.edge) && helper.Equals(other.helper);
+    }
+  }
+}
---
+++ unity/Assets/Scripts/Util/Algorithms/Monotone/Monotone.cs
@@ -0,0 +1,39 @@
+﻿namespace Util.Monotone
+{
+  using System.Collections;
+  using System.Collections.Generic;
+  using UnityEngine;
+  using Util.DataStructures.BST;
+  using Util.DataStructures.Queue;
+  using Puzzle;
+
+  public class Monotone : MonoBehaviour
+  {
+    public List<Polygon> MakeMonotone(Polygon input)
+    {
+      // Initialze the event queue with all points.
+      IPriorityQueue<PolygonPoint> events = new BinaryHeap<PolygonPoint>(YComparer.Instance);
+      foreach (PolygonPoint p in input.points)
+      {
+        events.Push(p);
+      }
+
+      // Initialize the status structure as an empty BST;
+      IBST<IntersectingComponent> status = new AATree<IntersectingComponent>();
+
+      // Continue while there are remaining events
+      while (events.Count > 0)
+      {
+        PolygonPoint p = events.Pop();
+        HandleVertex(status, p);
+      }
+
+      return null;
+    }
+
+    private void HandleVertex(IBST<IntersectingComponent> status, PolygonPoint p)
+    {
+
+    }
+  }
+}
