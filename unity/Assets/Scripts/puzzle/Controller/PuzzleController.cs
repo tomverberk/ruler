7f4b26263ad1dda79eb7aef1da74d14ba57d9a15
Tom <tomverberk@gmail.com>
2021-01-11 19:09:51 +0100

    added auxilery edges

--- unity/Assets/Scripts/puzzle/Controller/PuzzleController.cs
+++ unity/Assets/Scripts/puzzle/Controller/PuzzleController.cs
@@ -24,6 +24,10 @@
         [SerializeField]
         public GameObject m_edgeMeshPrefab;
         [SerializeField]
+        public GameObject m_demonstrationEdgePrefab;
+        [SerializeField]
+        public GameObject m_demonstrationMonotomeEdgePrefab;
+        [SerializeField]
         private GameObject m_pointPrefab;
         [SerializeField]
         private List<GameObject> m_triangleMeshPrefabList;
@@ -40,11 +44,21 @@
         internal Vector3 carryOffset = Vector3.zero;
         private List<Vector2> m_points = new List<Vector2>();
         private List<PuzzlePiece> pieces = new List<PuzzlePiece>();
+        private List<Polygon2D> monotone = new List<Polygon2D>();
 
         private List<GameObject> instantObjects;
 
+        private List<GameObject> monotomeEdges;
+        private List<GameObject> allEdges;
+
+
+        private List<List<Polygon2D>> allTriangles = new List<List<Polygon2D>>();
+
         protected int m_levelCounter = 0;
         protected int colorCounter = 0;
+
+        private bool m_wasPressed;
+        private bool t_wasPressed;
 
 
         void Start()
@@ -53,6 +67,8 @@
 
             // get unity objects
             instantObjects = new List<GameObject>();
+            monotomeEdges = new List<GameObject>();
+            allEdges = new List<GameObject>();
             m_points = new List<Vector2>();
 
             InitLevel();
@@ -61,7 +77,7 @@
 
         public void InitLevel()
         {
-            print("creating level" + m_levelCounter);
+            print("creating level " + m_levelCounter);
             if (m_levelCounter >= m_levels.Count)
             {
                 SceneManager.LoadScene(m_victoryScene);
@@ -83,7 +99,7 @@
             Polygon2D testPolygon = new Polygon2D(m_points);
             drawEdgesOfPolygon(testPolygon.Segments);
 
-            List<Polygon2D> monotone = Monotone.MakeMonotone(testPolygon);
+            monotone = Monotone.MakeMonotone(testPolygon);
             foreach (Polygon2D p in monotone)
             {
                 List<Polygon2D> triangles = Triangulate.TriangulatePoly(p);
@@ -92,6 +108,7 @@
                     PuzzlePiece piece = CreatePiece(t);
                     pieces.Add(piece);
                 }
+                allTriangles.Add(triangles);
             }
 
             // Set initial position random
@@ -105,6 +122,9 @@
             }
 
             // disable advance button
+            m_wasPressed = false;
+            t_wasPressed = false;
+
             m_advanceButton.Disable();
 
         }
@@ -153,16 +173,101 @@
                 print("Check the solution");
                 CheckSolution();
             }
+
+            if (Input.GetKeyDown("m"))
+            {
+                if (!m_wasPressed)
+                {
+                    print("Show monotome edges");
+                    m_wasPressed = true;
+                    foreach (Polygon2D p in monotone)
+                    {
+                        drawAuxileryMonotomeEdges(p.Segments);
+                    }
+                }
+                else
+                {
+                    print("Don't show monotome edges");
+                    m_wasPressed = false;
+                    foreach (var obj in monotomeEdges)
+                    {
+                        DestroyImmediate(obj);
+                    }
+                }
+            }
+
+            if (Input.GetKeyDown("t"))
+            {
+                if (!t_wasPressed)
+                {
+                    print("Show all edges");
+                    t_wasPressed = true;
+                    foreach (var list in allTriangles)
+                    {
+                        foreach (var p in list)
+                        {
+                            drawAllAuxileryEdges(p.Segments);
+                        }
+                    }
+                }
+                else
+                {
+                    print("Don't show all edges");
+                    t_wasPressed = false;
+                    foreach (var obj in allEdges)
+                    {
+                        // destroy immediate
+                        // since controller will search for existing objects afterwards
+                        DestroyImmediate(obj);
+                    }
+                }
+            }
         }
 
+        //Int gameObjectList represents the list to which the edge should be added.
+        // gameObjectList == 0 is the normal gameObjectList
+        //                == 1 is the monotomeEdges gameObjectList
+        //                == 2 is the allEdges gameObjectList
         public void drawEdgesOfPolygon(ICollection<LineSegment> edges)
         {
             foreach (LineSegment edge in edges)
             {
-
                 var drawedEdge = Instantiate(m_edgeMeshPrefab, Vector3.forward, Quaternion.identity) as GameObject;
                 drawedEdge.transform.parent = this.transform;
                 instantObjects.Add(drawedEdge);
+                
+                var roadmeshScript = drawedEdge.GetComponent<ReshapingMesh>();
+
+                roadmeshScript.CreateNewMesh(edge.Point1, edge.Point2);
+            }
+        }
+
+        //Int gameObjectList represents the list to which the edge should be added.
+        // gameObjectList == 0 is the allEdges gameObjectList
+        //                == 1 is the monotomeEdges gameObjectList
+        public void drawAuxileryMonotomeEdges(ICollection<LineSegment> edges)
+        {
+            foreach (LineSegment edge in edges)
+            {
+                var drawedEdge = Instantiate(m_demonstrationMonotomeEdgePrefab, Vector3.forward, Quaternion.identity) as GameObject;
+                drawedEdge.transform.parent = this.transform;
+
+                monotomeEdges.Add(drawedEdge);
+
+                var roadmeshScript = drawedEdge.GetComponent<ReshapingMesh>();
+
+                roadmeshScript.CreateNewMesh(edge.Point1, edge.Point2);
+            }
+        }
+
+        public void drawAllAuxileryEdges(ICollection<LineSegment> edges)
+        {
+            foreach (LineSegment edge in edges)
+            {
+                var drawedEdge = Instantiate(m_demonstrationEdgePrefab, Vector3.forward, Quaternion.identity) as GameObject;
+                drawedEdge.transform.parent = this.transform;
+
+                allEdges.Add(drawedEdge);
 
                 var roadmeshScript = drawedEdge.GetComponent<ReshapingMesh>();
 
@@ -210,10 +315,25 @@
                 // since controller will search for existing objects afterwards
                 DestroyImmediate(obj);
             }
+            foreach (var obj in monotomeEdges)
+            {
+                // destroy immediate
+                // since controller will search for existing objects afterwards
+                DestroyImmediate(obj);
+            }
+            foreach (var obj in allEdges)
+            {
+                // destroy immediate
+                // since controller will search for existing objects afterwards
+                DestroyImmediate(obj);
+            }
 
             instantObjects.Clear();
             m_points.Clear();
             pieces.Clear();
+            monotone.Clear();
+            allTriangles.Clear();
+
         }
     }
 }
